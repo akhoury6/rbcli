927b0806aa8c353b82d9fee2968486091035a9ab
Andrew Khoury <akhoury@live.com>
2018-07-23 02:11:19 -0400

    Bash convenience wrapper

---
+++ lib-sh/lib-rbcli.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+## Log message
+function message {
+	if [ "$2" == "err" ]; then
+		COLOR='\033[00;31m'
+	else
+
+		COLOR='\033[00;32m'
+	fi
+
+	if [ "$2" == "no_newline" ]; then
+		printf "${COLOR}[RBCLI-Setup] ${1}\033[0m"
+	else
+		echo -e "${COLOR}[RBCLI-Setup] ${1}\033[0m"
+	fi
+}
+
+## Detect OS
+function detect_os {
+	unameOut="$(uname -s)"
+	case "${unameOut}" in
+		Linux*)     machine=Linux;;
+		Darwin*)    machine=Darwin;;
+		CYGWIN*)    machine=Cygwin;;
+		MINGW*)     machine=MinGw;;
+		*)          machine="UNKNOWN:${unameOut}"
+	esac
+	echo ${machine}
+}
+
+## Install JQ
+function install_jq {
+	case "$(detect_os)}" in
+		Linux)
+			_tmpdir=$(mktemp -d)
+			_pwd=$(pwd)
+			cd $_tmpdir
+			curl -sOL "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64"
+			mv jq-linux64 /usr/bin/jq
+			cd $_pwd
+			rm -rf $_tmpdir
+			;;
+		Darwin*)
+			message "Apple OSX Detected; homebrew must be installed to continue. Continue? (Y/n): " "no_newline"
+			read -n1 _answer
+			printf "\n"
+			if [[ "${_answer}" =~ ^(y|Y)$ ]]; then
+				brew install jq
+			else
+				message "Installation aborted."
+				exit 0
+			fi
+			;;
+		*)
+			message "Sorry, your machine type ($(detect_os)) is not supported. This script can only install JQ automatically on either Linux or OSX."
+			message "Please visit https://stedolan.github.io/jq/ to install JQ manually on your system."
+			exit 0
+	esac
+}
+
+
+# We need to make sure JQ is installed so that we can parse variables
+which jq &> /dev/null
+_RESULT=$?
+if [[ "${_RESULT}" -ne 0 ]]; then
+	message "Application JQ not found on local system."
+	message "Installing JQ. Sudo access required, please input password if prompted."
+	install_jq
+fi
+
+
+######
+## RBCLI
+######
+# This function provides an interface to the variables exposed by RBCli
+function rbcli {
+	_vargroup=$1
+	_var=$2
+
+	case "${_vargroup}" in
+		params)
+			_groupname='__RBCLI_PARAMS'
+		;;
+		args)
+			_groupname='__RBCLI_ARGS'
+		;;
+		global_opts)
+			_groupname='__RBCLI_GLOBAL'
+		;;
+		config)
+			_groupname='__RBCLI_CONFIG'
+		;;
+		myvars)
+			_groupname='__RBCLI_MYVARS'
+		;;
+		*)
+			message "Invalid RBCLI variable reference. Please reference variables from one of the following groups: params, args, global_opts, config, commandvars." "err"
+			exit 1
+	esac
+	echo $(echo ${!_groupname} | jq -r "${2}")
+}
+export -f rbcli
+
--- skeletons/project/application/commands/scripts/script.sh
+++ skeletons/project/application/commands/scripts/script.sh
@@ -1,23 +1,55 @@
 #!/usr/bin/env bash
+#
+###
+# This is the RBCli script for the command `<%= @vars[:name] %>`
+###
+#
+# You can access RBCli's variables via the `rbcli` command, which is imported by this line:
+source $(echo $(cd "$(dirname $(gem which rbcli))/../lib-sh" && pwd)/lib-rbcli.sh)
+#
+# The `rbcli` function is actually a wrapper around `jq` (https://stedolan.github.io/jq/). It can automatically
+# jq on users' machines if running Linux or OSX, and users have sudo access. If not, users must install it manually.
+# This is because behind the scenes, RBCli passes in the variable data by populating evironment variables with JSON.
+#
+# You can view the entire JSON structures with:
+#
+#    rbcli params
+#    rbcli args
+#    rbcli global_opts
+#    rbcli config
+#    rbcli myvars
+#
+# And to access specific data within that json, you can add some jq syntax as shown below. The first two examples show
+# how to get data from a nested hash, and the last example shows how to select an item from an array.
+#
+#     echo "Log Level:  $(rbcli config .logger.log_level)"
+#     echo "Log Target: $(rbcli config .logger.log_target)"
+#     echo "First Argument (if passed): $(rbcli args .[0])"
+#
+# If you wish to parse the variables manually and remove the dependency on JQ, you can comment out the line above
+# and access these variables directly:
+#
+#    __RBCLI_PARAMS
+#    __RBCLI_ARGS
+#    __RBCLI_GLOBAL
+#    __RBCLI_CONFIG
+#    __RBCLI_MYVARS
+#
+###
+#
 
-###
-# This is the RBCli script for the command {{**CMDNAME**}}
-###
-#
-# You can find RBCli's params, args, global_opts, and config through environment
-# variables. They are passed in the formats:
-#
-#   __PARAMS_<param_name>
-#   __ARGS_<arg_name>
-#   __GOBAL <global_opt_name>
-#   __CONFIG <config_name>
-#
-# If any of the options given had nested values in your RBCli application they
-# will be passed as JSON. You can parse them using jq ( https://stedolan.github.io/jq/ )
-#
-# If you specified any environment variables to be set manually, they will be found unmodified.
-#
-###
-#
-
-env | grep "^__PARAMS\|^__ARGS\|^__GLOBAL\|^__CONFIG"
+echo "------Params------"
+rbcli params
+echo "------Args------"
+rbcli args
+echo "------Global Opts------"
+rbcli global_opts
+echo "------Config------"
+rbcli config
+echo "------MyVars------"
+rbcli myvars
+echo ""
+echo "Usage Examples:"
+echo "Log Level:  $(rbcli config .logger.log_level)"
+echo "Log Target: $(rbcli config .logger.log_target)"
+echo "First Argument (if passed): $(rbcli args .[0])"

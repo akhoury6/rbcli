0cd8965ddbd47aef93b4ece03999917e1cf19cb2
Andrew Khoury <akhoury@live.com>
2018-08-05 03:51:50 -0400

    GPLv3 Copyright Notice

--- docs-src/docs/imported/quick_reference.md
+++ docs-src/docs/imported/quick_reference.md
@@ -109,27 +109,21 @@ Users can generate a config file, complete with help text, by running your tool 
 
 ## Logging
 
-Logging with RBCli is straightforward - it looks at the config file for logging settings, and instantiates a single, globally accessible [Logger](https://ruby-doc.org/stdlib-2.4.0/libdoc/logger/rdoc/Logger.html)` object. You can access it as such:
+RBCli's logger is configured in `config/logging.rb`.
+
+```ruby
+log_level :info
+log_target 'stderr'
+```
+
+Then it can be accessed when writing your commands via:
 
 ```ruby
 Rbcli::log.info { 'These logs can go to STDERR, STDOUT, or a file' }
 ```
 
-Default values can be set in the configurate DSL:
+The user will also be able to change the log level and target via their config file, if it is enabled.
 
-```ruby
-log_level :info                                                        # (Optional) Set the default log_level for users. 0-5, or DEBUG < INFO < WARN < ERROR < FATAL < UNKNOWN
-log_target 'stderr'                                                    # (Optional) Set the target for logs. Valid values are STDOUT, STDERR, or a file path (as strings)
-```
-
-Two configuration options will always be placed in a user's YAML file to override defaults: 
-
-```yaml
-# Log Settings
-logger:
-  log_level: warn              # 0-5, or DEBUG < INFO < WARN < ERROR < FATAL < UNKNOWN
-  log_target: stderr           # STDOUT, STDERR, or a file path
-```
 
 ## Automatic Update Check
 
@@ -148,7 +142,7 @@ The gem is available as open source under the terms of the [GPLv3](https://githu
 
 ## Full Documentation
 
-[You can find the full documentation for RBCli Here.][documentation_home]
+[You can find the Official Documentation for RBCli Here.][documentation_home]
 
 
 [documentation_home]: https://akhoury6.github.com/rbcli
--- lib/rbcli/util/hash_deep_symbolize.rb
+++ lib/rbcli/util/hash_deep_symbolize.rb
@@ -1,28 +1,49 @@
+##################################################################################
+#     RBCli -- A framework for developing command line applications in Ruby      #
+#     Copyright (C) 2018 Andrew Khoury                                           #
+#                                                                                #
+#     This program is free software: you can redistribute it and/or modify       #
+#     it under the terms of the GNU General Public License as published by       #
+#     the Free Software Foundation, either version 3 of the License, or          #
+#     (at your option) any later version.                                        #
+#                                                                                #
+#     This program is distributed in the hope that it will be useful,            #
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#     GNU General Public License for more details.                               #
+#                                                                                #
+#     You should have received a copy of the GNU General Public License          #
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
+#                                                                                #
+#     For questions regarding licensing, please contact andrew@bluenex.us        #
+##################################################################################
+
+
 ##
 # Functions to convert hash keys to all symbols or all strings
 ##
 class Hash
-  def deep_symbolize! hsh = nil
-    hsh ||= self
-    hsh.keys.each do |k|
-      if k.is_a? String
-        hsh[k.to_sym] = hsh[k]
-        hsh.delete k
-      end
-      deep_symbolize! hsh[k.to_sym] if hsh[k.to_sym].is_a? Hash
-    end
-    hsh
-  end
+	def deep_symbolize! hsh = nil
+		hsh ||= self
+		hsh.keys.each do |k|
+			if k.is_a? String
+				hsh[k.to_sym] = hsh[k]
+				hsh.delete k
+			end
+			deep_symbolize! hsh[k.to_sym] if hsh[k.to_sym].is_a? Hash
+		end
+		hsh
+	end
 
-  def deep_stringify! hsh = nil
-    hsh ||= self
-    hsh.keys.each do |k|
-      if k.is_a? Symbol
-        hsh[k.to_s] = hsh[k]
-        hsh.delete k
-      end
-      deep_stringify! hsh[k.to_s] if hsh[k.to_s].is_a? Hash
-    end
-    hsh
-  end
+	def deep_stringify! hsh = nil
+		hsh ||= self
+		hsh.keys.each do |k|
+			if k.is_a? Symbol
+				hsh[k.to_s] = hsh[k]
+				hsh.delete k
+			end
+			deep_stringify! hsh[k.to_s] if hsh[k.to_s].is_a? Hash
+		end
+		hsh
+	end
 end

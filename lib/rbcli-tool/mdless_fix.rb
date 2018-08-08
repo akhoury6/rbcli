fe5e57e03d85543c23a2ae3d9358010ab4a57a3c
Andrew Khoury <akhoury@live.com>
2018-08-08 12:08:22 -0400

    Copyright Text

--- lib/rbcli-tool/mdless_fix.rb
+++ lib/rbcli-tool/mdless_fix.rb
@@ -15,7 +15,7 @@
 #     You should have received a copy of the GNU General Public License          #
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
 #                                                                                #
-#     For questions regarding licensing, please contact andrew@blacknex.us        #
+#     For questions regarding licensing, please contact andrew@blacknex.us       #
 ##################################################################################
 
 require 'mdless'
